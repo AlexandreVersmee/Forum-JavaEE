d23891aab4338cd76918e2f30b41ad1992b40e5b
Maxence Bovier-Lapierre <maxi22-@hotmail.fr>
2015-11-01 03:48:12 +0100

    début  tableau pour admin validation
    ATTENTION code en travaux dans administrationinscription java

--- Forum-Bovier-Versmee/src/main/java/ca/uqar/forum/controller/AdministrationInscription.java
+++ Forum-Bovier-Versmee/src/main/java/ca/uqar/forum/controller/AdministrationInscription.java
@@ -2,15 +2,33 @@ package ca.uqar.forum.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import ca.uqar.forum.entities.Membre;
+
+import com.orange.OSAC.controller.AdministrationOrigineController;
+import com.orange.OSAC.controller.AdministrationOrigineController.Action;
+import com.orange.OSAC.entities.Origine;
+import com.orange.OSAC.entities.User;
 
 @Controller
 @RequestMapping(value="/administration-inscriptions")
 public class AdministrationInscription {
+	/* Debug */
+	private final static Logger logger = LoggerFactory.getLogger(AdministrationInscription.class);
+
 	/*
 	###############################
 	#                             #
@@ -37,4 +55,25 @@ public class AdministrationInscription {
 	#                             #
 	###############################
 	*/
+	@RequestMapping(value = "/{id}", method = RequestMethod.POST)
+	public String deleteOriginForm(@Valid @ModelAttribute(value="originToDelete") Origine originTemp,
+			BindingResult result, ModelMap model, HttpSession session,final RedirectAttributes redirectAttributes)
+	{
+		logger.debug("Delete NEW ORIGIN POST");
+		Membre userPref		= (Membre) session.getAttribute("userSession");
+		
+		/* Default Methods */
+		setDefaultViewVariables(pageable, model, null, Action.DELETE, userPref.getPreference());
+					
+		if (result.hasErrors()){
+			logger.debug("ERROR ON SUBMIT - UpdateOrigin (Number error :{}), {}",result.getErrorCount(), result.getAllErrors().get(0).getDefaultMessage());
+			model.addAttribute("ERROR_MESSAGE","L'origine "+originTemp.getName()+" n'a pas Ã©tÃ© ajoutÃ© en base de donnÃ©e, une erreur est survenue.");
+			return ("administrationOrigin");
+		}
+		
+		origineService.deleteOrigin(originTemp);	
+		
+		redirectAttributes.addFlashAttribute("SUCCESS_MESSAGE","L'origine "+originTemp.getName()+" a bien Ã©tÃ© supprimÃ© en base de donnÃ©e.");
+		return ("redirect:/administration/origines");		
+	}
 }
--- Forum-Bovier-Versmee/src/main/webapp/WEB-INF/views/administrationInscription.jsp
+++ Forum-Bovier-Versmee/src/main/webapp/WEB-INF/views/administrationInscription.jsp
@@ -7,30 +7,50 @@
 		<table>
 			<thead>
 			<tr>
-				<th>id</th>
+				<th>Nom</th>
 				<th>Pseudo</th>
 				<th>Adresse email</th>
-				<th>Date de dernière connexion</th>
+				<th>Ip</th>
 				<th class="text-center">Action</th>
 			</tr>
 			</thead>
-			<tbody>
-				<tr>
-					<td>t</td>
-					<td>e</td>
-					<td>s</td>
-					<td>t</td>
-					<td>t</td>
-				</tr>
-				<tr>
-					<td>t</td>
-					<td>e</td>
-					<td>s</td>
-					<td>t</td>
-					<td>t</td>
-				</tr>
-			</tbody>
-		</table>		
-	</div>
+            	<tbody>
+                	<c:forEach var="item" items="${membreList}" varStatus="status">
+                    	<jsp:useBean id="status" type="javax.servlet.jsp.jstl.core.LoopTagStatus"/>
+                    	<c:choose>
+                    		<c:when test="<%=status.getCount()%2==0%>">
+                            	<tr>
+                                	<td><c:out value="${item.name}"/></td>
+                                	<td><c:out value="${item.pseudo}"/></td>
+                                	<td><c:out value="${item.email}"/></td>
+                                	<td><c:out value="${item.ip}"/></td>
+                                	<td>
+                                    	<a href="/Forum-Bovier-Versmee/administration-inscriptions/${item.id}">Valider</a>
+                                	</td>
+                            	</tr>
+                        	</c:when>
+                        	<c:otherwise>
+                            	<tr>
+                                	<td><c:out value="${item.name}"/></td>
+                                	<td><c:out value="${item.pseudo}"/></td>
+                                	<td><c:out value="${item.email}"/></td>
+                                	<td><c:out value="${item.ip}"/></td>
+                                	<td>
+                                    	<a href="/Forum-Bovier-Versmee/administration-inscriptions/${item.id}">Valider</a>
+                                	</td>
+                            	</tr>
+                        	</c:otherwise>
+                    	</c:choose>
+                	</c:forEach>
+                </tbody>
+            </table>
+            <c:if test="${empty membreList}">
+            <div class="row">
+                <div class="large-12 columns text-center grey">
+                    <i>Pas d'utilisateurs</i>
+                </div>
+            </div>
+            </c:if>
+            	</div>
 </div>
 </body:base_layout>
